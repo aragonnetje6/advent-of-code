11671f155b4ff25e2d34b0dadf7c817d592c0f74
Twan Stok <t.s.stok@student.utwente.nl>
2022-12-02 22:02:40 +0100

    factored out input processing

--- ac2022/src/p01.rs
+++ ac2022/src/p01.rs
@@ -1,4 +1,11 @@
+use std::iter::Map;
+use std::str::Split;
+
 pub fn part_1(input: &str) -> u32 {
+    process_input(input).max().unwrap()
+}
+
+fn process_input(input: &str) -> Map<Map<Split<&str>, fn(_) -> _>, fn(Vec<u32>)> {
     input
         .split("\n\n")
         .map(|x| {
@@ -7,20 +14,10 @@ pub fn part_1(input: &str) -> u32 {
                 .collect()
         })
         .map(|sublist: Vec<u32>| sublist.iter().sum())
-        .max()
-        .unwrap()
 }
 
 pub fn part_2(input: &str) -> u32 {
-    let mut sums: Vec<u32> = input
-        .split("\n\n")
-        .map(|x| {
-            x.split('\n')
-                .map(|x| x.parse().unwrap_or_default())
-                .collect()
-        })
-        .map(|sublist: Vec<u32>| sublist.iter().sum())
-        .collect();
+    let mut sums: Vec<u32> = process_input(input).collect();
     sums.sort();
     sums.iter().rev().take(3).sum()
 }
