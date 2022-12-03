b4e2cb523e2288b050103e9c239c6f029325e904
Twan Stok <t.s.stok@student.utwente.nl>
2022-12-03 02:01:51 +0100

    solved 2022 po2

--- ac2022/src/p01.rs
+++ ac2022/src/p01.rs
@@ -1,23 +1,21 @@
-use std::iter::Map;
-use std::str::Split;
-
-pub fn part_1(input: &str) -> u32 {
-    process_input(input).max().unwrap()
+pub fn part1(input: &str) -> u32 {
+    *process_input(input).iter().max().unwrap()
 }
 
-fn process_input(input: &str) -> Map<Map<Split<&str>, fn(_) -> _>, fn(Vec<u32>)> {
+fn process_input(input: &str) -> Vec<u32> {
     input
         .split("\n\n")
         .map(|x| {
             x.split('\n')
-                .map(|x| x.parse().unwrap_or_default())
+                .map(|x| x.parse::<u32>().unwrap_or_default())
                 .collect()
         })
         .map(|sublist: Vec<u32>| sublist.iter().sum())
+        .collect()
 }
 
-pub fn part_2(input: &str) -> u32 {
-    let mut sums: Vec<u32> = process_input(input).collect();
+pub fn part2(input: &str) -> u32 {
+    let mut sums: Vec<u32> = process_input(input);
     sums.sort();
     sums.iter().rev().take(3).sum()
 }
---
+++ ac2022/src/p02.rs
@@ -0,0 +1,150 @@
+type Recommendation = (Move, Move);
+type Recommendation2 = (Move, Outcome);
+
+#[derive(Eq, PartialEq, Clone, Debug)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Eq, PartialEq, Clone, Debug)]
+enum Outcome {
+    Win,
+    Draw,
+    Loss,
+}
+
+pub fn part1(input: &str) -> u32 {
+    let data = process_input(input);
+    data.iter()
+        .map(|recommend| get_move_score(recommend) + get_outcome_score(&get_outcome(recommend)))
+        .sum()
+}
+
+pub fn part2(input: &str) -> u32 {
+    let data = process_input_2(input);
+    data.iter()
+        .map(translate)
+        .map(|recommend| get_move_score(&recommend) + get_outcome_score(&get_outcome(&recommend)))
+        .sum()
+
+}
+
+fn translate(rec: &Recommendation2) -> Recommendation {
+    match rec.1 {
+        Outcome::Win => (rec.0.clone(), defeats(&rec.0)),
+        Outcome::Draw => (rec.0.clone(), rec.0.clone()),
+        Outcome::Loss => (rec.0.clone(), defeated_by(&rec.0))
+    }
+}
+
+const fn get_move_score(rec: &Recommendation) -> u32 {
+    match rec.1 {
+        Move::Rock => 1,
+        Move::Paper => 2,
+        Move::Scissors => 3,
+    }
+}
+
+const fn defeats(x: &Move) -> Move {
+    match x {
+        Move::Rock => Move::Paper,
+        Move::Paper => Move::Scissors,
+        Move::Scissors => Move::Rock,
+    }
+}
+
+const fn defeated_by(x: &Move) -> Move {
+    match x {
+        Move::Rock => Move::Scissors,
+        Move::Paper => Move::Rock,
+        Move::Scissors => Move::Paper,
+    }
+}
+
+fn get_outcome(rec: &Recommendation) -> Outcome {
+    if rec.0 == rec.1 {
+        Outcome::Draw
+    } else if rec.1 == defeats(&rec.0) {
+        Outcome::Win
+    } else {
+        Outcome::Loss
+    }
+}
+
+const fn get_outcome_score(outcome: &Outcome) -> u32 {
+    match outcome {
+        Outcome::Win => 6,
+        Outcome::Draw => 3,
+        Outcome::Loss => 0,
+    }
+}
+
+fn process_input(input: &str) -> Vec<Recommendation> {
+    input
+        .split('\n')
+        .map(|line| {
+            line.split(' ')
+                .filter_map(|char| match char {
+                    "A" | "X" => Some(Move::Rock),
+                    "B" | "Y" => Some(Move::Paper),
+                    "C" | "Z" => Some(Move::Scissors),
+                    _ => None,
+                })
+                .collect::<Vec<Move>>()
+        })
+        .filter(|x| !x.is_empty())
+        .map(|v| (v[0].clone(), v[1].clone()))
+        .collect::<Vec<Recommendation>>()
+}
+
+fn process_input_2(input: &str) -> Vec<Recommendation2> {
+    process_input(input)
+        .iter()
+        .map(|(mov1, mov2)| {
+            (
+                mov1.clone(),
+                match mov2 {
+                    Move::Rock => Outcome::Loss,
+                    Move::Paper => Outcome::Draw,
+                    Move::Scissors => Outcome::Win,
+                },
+            )
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(get_outcome_score(&get_outcome(&(Move::Rock, Move::Paper))), 6);
+        assert_eq!(get_outcome_score(&get_outcome(&(Move::Paper, Move::Rock))), 0);
+        assert_eq!(get_outcome_score(&get_outcome(&(Move::Scissors, Move::Scissors))), 3);
+        assert_eq!(get_move_score(&(Move::Rock, Move::Paper)), 2);
+        assert_eq!(get_move_score(&(Move::Paper, Move::Rock)), 1);
+        assert_eq!(get_move_score(&(Move::Scissors, Move::Scissors)), 3);
+        assert_eq!(part1("A Y\nB X\nC Z\n"), 15);
+        dbg!(process_input("A X\n"));
+        assert_eq!(part1("A X\n"), 3 + 1);
+        assert_eq!(part1("A Y\n"), 6 + 2);
+        assert_eq!(part1("A Z\n"), 0 + 3);
+        assert_eq!(part1("B X\n"), 0 + 1);
+        assert_eq!(part1("B Y\n"), 3 + 2);
+        assert_eq!(part1("B Z\n"), 6 + 3);
+        assert_eq!(part1("C X\n"), 6 + 1);
+        assert_eq!(part1("C Y\n"), 0 + 2);
+        assert_eq!(part1("C Z\n"), 3 + 3);
+        assert_eq!(
+            part1("A X\nA Y\nA Z\nB X\nB Y\nB Z\nC X\nC Y\nC Z\n"),
+            4 + 8 + 3 + 1 + 5 + 9 + 7 + 2 + 6
+        );
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("A Y\nB X\nC Z\n"), 12);
+    }
+}
