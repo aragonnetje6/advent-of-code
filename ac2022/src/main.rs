75fbc0409bf308049dedb8347ed5c5264973f66b
Twan Stok <t.s.stok@student.utwente.nl>
2022-12-04 23:49:55 +0100

    2022 p04

--- ac2022/src/main.rs
+++ ac2022/src/main.rs
@@ -3,6 +3,7 @@ use lib::wrapper;
 mod p01;
 mod p02;
 mod p03;
+mod p04;
 
 fn main() {
     wrapper(2022, 1, 1, p01::part1);
@@ -11,4 +12,6 @@ fn main() {
     wrapper(2022, 2, 2, p02::part2);
     wrapper(2022, 3, 1, p03::part1);
     wrapper(2022, 3, 2, p03::part2);
+    wrapper(2022, 4, 1, p04::part1);
+    wrapper(2022, 4, 2, p04::part2);
 }
---
+++ ac2022/src/p04.rs
@@ -0,0 +1,65 @@
+pub fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(|line| {
+            let mut line_iter = line.split(',').map(|elf| {
+                let mut iter = elf.split('-');
+                (
+                    iter.next().unwrap().parse::<u32>().unwrap(),
+                    iter.next().unwrap().parse::<u32>().unwrap(),
+                )
+            });
+            (line_iter.next().unwrap(), line_iter.next().unwrap())
+        })
+        .filter(|((elf1_low, elf1_high), (elf2_low, elf2_high))| {
+            (elf1_low <= elf2_low && elf1_high >= elf2_high)
+                || (elf2_low <= elf1_low && elf2_high >= elf1_high)
+        })
+        .count()
+}
+
+pub fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(|line| {
+            let mut line_iter = line.split(',').map(|elf| {
+                let mut iter = elf.split('-');
+                (
+                    iter.next().unwrap().parse::<u32>().unwrap(),
+                    iter.next().unwrap().parse::<u32>().unwrap(),
+                )
+            });
+            (line_iter.next().unwrap(), line_iter.next().unwrap())
+        })
+        .filter(|((elf1_low, elf1_high), (elf2_low, elf2_high))| {
+            (elf1_low <= elf2_low && elf1_high >= elf2_high)
+                || (elf2_low <= elf1_low && elf2_high >= elf1_high)
+                || (elf1_low <= elf2_low && elf1_high >= elf2_low)
+                || (elf1_low <= elf2_high && elf1_high >= elf2_high)
+        })
+        .count()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const DATA: &str = r"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(DATA), 2);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(DATA), 4);
+    }
+}
